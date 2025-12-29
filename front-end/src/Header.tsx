fc8849c0dcd82b647883bd5a2fea99e5befb9369
CRD-digital <carloshazard@gmail.com>
2025-12-29 16:35:33 -0300

    feat (prettier)
    instalando prettier

--- front-end/src/Header.tsx
+++ front-end/src/Header.tsx
@@ -1,12 +1,14 @@
 const Header = () => {
-    return (
-        <div className="bg-[#141614]">
-            <div className="w-full md:w-[737px] p-3 md:p-0 mx-auto flex items-center justify-between">
-                <img src="./logo.png" alt="" />
-                <button className="bg-[#F2DAAC] w-[130px] h-[35px] flex items-center justify-center rounded-sm cursor-pointer">Entrar</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="bg-[#141614]">
+      <div className="mx-auto flex w-full items-center justify-between p-3 md:w-[737px] md:p-0">
+        <img src="./logo.png" alt="" />
+        <button className="flex h-[35px] w-[130px] cursor-pointer items-center justify-center rounded-sm bg-[#F2DAAC]">
+          Entrar
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
